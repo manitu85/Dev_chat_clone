1ae451478e1087505448d22939c855ab43e38a84
manitu85 <manitu.psy@gmail.com>
2019-10-12 18:49:01 +0200

    '#'

--- src/Components/Messages/MessageForm.js
+++ src/Components/Messages/MessageForm.js
@@ -1,36 +1,49 @@
 import React, { Component } from 'react'
-import { Segment, Input, Button } from 'semantic-ui-react'
 import firebase from '../../firebase'
+import uuidv4 from 'uuid/v4';
+import { Segment, Input, Button } from 'semantic-ui-react'
+
+import FileModal from './FileModal';
+import ProggresBar from './ProggresBar';
 
 export class MessageForm extends Component {
 
   state = {
+    storageRef: firebase.storage().ref(),
+    uploadTask: null,
+    uploadState: '',
+    percentUploaded: 0,
     message: '',
     errors: [],
     channel: this.props.currentChannel,
     user: this.props.currentUser,
     loadign: false,
+    modal: false
   }
 
-
+  openModal = ()=> this.setState({ modal: true})
+  closeModal = ()=> this.setState({ modal: false})
 
   handleOnChange = e => {
     const { name, value } = e.target
     this.setState({ [name] : value })
   }
 
-  createMessage = () => {
-    const { user } = this.state
+  createMessage = (fileUrl = null) => {
     const message = {
       timestamp: firebase.database.ServerValue.TIMESTAMP,
       user: {
-        id: user.uid,
-        name: user.displayName,
-        avatar: user.photoURL
-      }, 
-      content: this.state.message
+        id: this.state.user.uid,
+        name: this.state.user.displayName,
+        avatar: this.state.user.photoURL
+      }
+    };
+    if (fileUrl !== null) {
+      message["image"] = fileUrl;
+    } else {
+      message["content"] = this.state.message;
     }
-    return message
+    return message;
   }
 
   sendMessage = () => {
@@ -65,9 +78,67 @@ export class MessageForm extends Component {
       })
     }
   }
+
+  uploadFile = (file, metadata) => {
+    const pathToUpload = this.state.channel.id
+    const ref = this.props.messagesRef
+    const filePath = `chat/public/${uuidv4().jpg}`
+
+    this.setState({
+      uploadState: 'uploading',
+      uploadTask: this.state.storageRef.child(filePath).put(file, metadata)
+    },
+      () => {
+       this.state.uploadTask.on('state_changed', snap => {
+         const percentUploaded = Math.round((snap.bytesTransferred / snap.totalBytes) * 100)
+         this.setState({ percentUploaded })
+       },
+        err => {
+          console.error(err)
+          this.setState({
+            errors: this.state.error.concat(err),
+            uploadState: 'error',
+            uploadTask: null
+          })
+        },
+         () => {
+           this.state.uploadTask.snapshot.ref
+             .getDownloadURL()
+             .then(downloadUrl => {
+               this.sendFileMessage(downloadUrl, ref, pathToUpload);
+             })
+             .catch(err => {
+               console.error(err);
+               this.setState({
+                 errors: this.state.errors.concat(err),
+                 uploadState: "error",
+                 uploadTask: null
+               });
+             });
+         }
+       ) 
+      }
+    )}
+
+  sendFileMessage = (fileUrl, ref, pathToUpload) => {
+    ref
+      .child(pathToUpload)
+      .push()
+      .set(this.createMessage(fileUrl))
+      .then(() => {
+        this.setState({ uploadState: "done" });
+      })
+      .catch(err => {
+        console.error(err);
+        this.setState({
+          errors: this.state.errors.concat(err)
+        });
+      });
+  };
   
   render() {
-    const { errors } = this.state
+    // prettier ignore
+    const { errors, message, loading, modal, uploadState, percentUploaded } = this.state
    
     return (
       <Segment className='message__form'>
@@ -78,6 +149,7 @@ export class MessageForm extends Component {
           label={<Button icon={'add'} />}
           labelPosition='left'
           placeholder='Write your message'
+          value={message}
           onChange={this.handleOnChange}
           className={
             errors.some(err => err.message.includes('message')) ? 'error' : ''
@@ -86,16 +158,27 @@ export class MessageForm extends Component {
         <Button.Group icon widths='2'>
           <Button 
             onClick={this.sendMessage}
+            disabled={loading}
             color='orange'
             content='Add Reply'
             labelPosition='left'
             icon='edit'
           />
           <Button 
+            onClick={this.openModal}
             color='teal'
             content='Upload Media'
             labelPosition='right'
             icon='cloud upload'
+          />
+          <FileModal
+            modal={modal}
+            closeModal={this.closeModal}
+            uploadFile={this.uploadFile}
+           />
+          <ProggresBar 
+            uploadState={uploadState}
+            percentUploaded={percentUploaded}
           />
         </Button.Group>
       </Segment>
--- src/Components/Messages/Messages.js
+++ src/Components/Messages/Messages.js
@@ -1,24 +1,62 @@
 import React, { Component } from 'react'
 import { Segment, Comment } from 'semantic-ui-react'
+import firebase from '../../firebase'
+
 import MessageHeader from './MessageHeader'
 import MessageForm from './MessageForm'
-import firebase from '../../firebase'
+import Message from "./Message";
 
 export class Messages extends Component {
 
   state = {
     messagesRef: firebase.database().ref('messages'),
+    messages: [],
+    messagesLoading: true,
     channel: this.props.currentChannel,
     user: this.props.currentUser
   }
+
+  componentDidMount() {
+    const { channel, user } = this.state;
+
+    if (channel && user) {
+      this.addListeners(channel.id);
+    }
+  }
+
+  addListeners = channelId => {
+    this.addMessageListener(channelId);
+  };
+
+  addMessageListener = channelId => {
+    let loadedMessages = [];
+    this.state.messagesRef.child(channelId).on("child_added", snap => {
+      loadedMessages.push(snap.val());
+      this.setState({
+        messages: loadedMessages,
+        messagesLoading: false
+      });
+    });
+  };
+
+  displayMessages = messages =>
+    messages.length > 0 &&
+      messages.map(message => (
+        <Message
+          key={message.timestamp}
+          message={message}
+          user={this.state.user}
+        />
+    ));
+
   render() {
-    const { messagesRef, channel, user } = this.state
+    const { messagesRef, messages, channel, user } = this.state
     return (
       <>
         <MessageHeader />
         <Segment>
           <Comment.Group className='messages'>
-            {/* Messages */}
+            {this.displayMessages(messages)}
           </Comment.Group>
         </Segment>
         <MessageForm 
